eead9935d0fe272d3ff3266153aaa9acdcc37971
temur-komilov <123106511+temur-komilov@users.noreply.github.com>
2023-06-26 12:14:35 +0500

    second

---
+++ src/components/navbar-section/navbarSection.vue
@@ -0,0 +1,57 @@
+<template lang="">
+    
+    <div class="container">
+        <nav class="quantic-navbar">
+            <ul class="navbar-items">
+          <li class="navbar_item"><a href="#">Наши премущество</a></li>
+          <li class="navbar_item"><a  class=" dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink"  data-bs-toggle="dropdown" >Наши курсы </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#"></a></li>
+            <li><a class="dropdown-item" href="#"></a></li>
+            <li><a class="dropdown-item" href="#"></a></li>
+          </ul>
+          </li>
+          <li class="navbar_item"><a href="#">О нас</a></li>
+          <li class="navbar_item"><a href="#">Коворкинг</a></li>
+          <li class="navbar_item"><a href="#">Бесплатный урок</a></li>
+          <li class="navbar_item"><a href="#">Работы наших учеников</a></li>
+          <li class="navbar_item"><a href="#" class=" dropdown-toggle" id="navbarDarkDropdownMenu"  data-bs-toggle="dropdown">Карьера</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
+                <li><a class="dropdown-item" href="#"></a></li>
+                <li><a class="dropdown-item" href="#"></a></li>
+                <li><a class="dropdown-item" href="#"></a></li>
+            </ul>
+          </li>
+          <li class="navbar_item"><a href="#">Отзывы</a></li>
+          <li class="navbar_item"><a href="#">Контакты</a></li>
+           </ul>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+.navbar-items {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+}
+
+.navbar-items a {
+    position: relative;
+    text-decoration: none;
+    color: rgba(255, 255, 255, 1);
+    font-family: 'Montserrat';
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 30.584px;
+}
+
+.navbar_item {
+    display: inline-block;
+}
+</style>
